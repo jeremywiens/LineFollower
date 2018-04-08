0a11e3b2d5793c12341f5e7336e724e05baf0847
jeremywiens <31671215+jeremywiens@users.noreply.github.com>
2018-04-08 15:27:53 -0700

    Update line-follower-template.ino

--- line-follower-template/line-follower-template.ino
+++ line-follower-template/line-follower-template.ino
@@ -13,13 +13,21 @@
 #define RIGHT 6
 #define MUCH_RIGHT  2
 
+#define test 7
+
+#define LPower 85
+#define RPower 85
+
+int left_acute = 0;
+int right_acute = 0;
+
 
 
 #define S_HIGH 900  //analog readings above 900 if line detected 
                     //digital readings will be HIGH
 #define S_LOW 50  // analog readings below 50 if no line is detected
                     // digital readings will be LOW
-
+int readings = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -34,18 +42,26 @@ void setup() {
 //controlling PWM controls speed by turning voltage high and low for //various frequencies 
 //for i.e pwm of 30% means voltage is high for 30% of the time and low //for 70% of the time
 void Forward(){
-    analogWrite (en_L,150); 
-    analogWrite (en_R,150); 
+    analogWrite (en_L,LPower); 
+    analogWrite (en_R,RPower); 
     digitalWrite(left_f, HIGH);
     digitalWrite(right_f, HIGH);
     digitalWrite(left_b, LOW);
     digitalWrite(right_b, LOW);
 }
 void HardLeft(){
-    analogWrite (en_L,150); 
-    analogWrite (en_R,150);
+    analogWrite (en_L,0); 
+    analogWrite (en_R,RPower);
     digitalWrite(left_f, LOW);
     digitalWrite(right_f, HIGH); 
+    digitalWrite(left_b, LOW);
+    digitalWrite(right_b, LOW);
+}
+void Left(){
+    analogWrite (en_L,LPower-50); 
+    analogWrite (en_R,RPower); 
+    digitalWrite(left_f, HIGH);
+    digitalWrite(right_f, HIGH);
     digitalWrite(left_b, LOW);
     digitalWrite(right_b, LOW);
 }
@@ -58,33 +74,41 @@ void Stop(){
 }
 
 void Back(){    
-  analogWrite (en_L,150); 
-    analogWrite (en_R,150); 
+  analogWrite (en_L,LPower); 
+    analogWrite (en_R,RPower); 
     digitalWrite(left_f, LOW);
     digitalWrite(right_f, LOW);
     digitalWrite(left_b, HIGH);
     digitalWrite(right_b, HIGH);
 
 }
+void Right(){
+    analogWrite (en_L,LPower); 
+    analogWrite (en_R,RPower-50); 
+    digitalWrite(left_f, HIGH);
+    digitalWrite(right_f, HIGH);
+    digitalWrite(left_b, LOW);
+    digitalWrite(right_b, LOW);
+}
 void HardRight(){
-    analogWrite (en_L,150); 
-    analogWrite (en_R,150); 
+    analogWrite (en_L,LPower); 
+    analogWrite (en_R,0); 
     digitalWrite(left_f, HIGH);
     digitalWrite(right_f, LOW);
     digitalWrite(left_b, LOW);
     digitalWrite(right_b, LOW);
 }
-void SightLeft(){      
-    analogWrite (en_L,75); 
-    analogWrite (en_R,150); 
+void SightLeft(){ 
+    analogWrite (en_L,0);     
+    analogWrite (en_R,RPower); 
     digitalWrite(left_f, HIGH);
     digitalWrite(right_f, HIGH);    
     digitalWrite(left_b, LOW);
     digitalWrite(right_b, LOW);
     }
 void SlightRight(){
-    analogWrite (en_L,150); 
-    analogWrite (en_R,75); 
+    analogWrite (en_L,LPower); 
+    analogWrite (en_R,0); 
     digitalWrite(left_f, HIGH);
     digitalWrite(right_f, HIGH);
     digitalWrite(left_b, LOW);
@@ -93,14 +117,21 @@ void SlightRight(){
 
 void loop (){
 
+    int testt = digitalRead(test);
+    
+
     int m_left=digitalRead(MUCH_LEFT);
     int left=digitalRead(LEFT);
-    int center= !digitalRead(CENTER);
+    int center= digitalRead(CENTER);
     int right=digitalRead(RIGHT);
     int m_right=digitalRead(MUCH_RIGHT);
 
-    //concating sensor readings together
-    int readings=m_left*10000+left*1000+center*100+right*10+m_right;
+    //concating sensor readings together 
+    int newReadings =m_left*10000+left*1000+center*100+right*10+m_right;
+    
+    if(newReadings != 0){
+       readings = newReadings;
+    }
     Serial.print("\n");
     Serial.print(m_left);
     Serial.print("\t");
@@ -116,7 +147,7 @@ void loop (){
    
     switch (readings) {
     case 10000:    // very left sensor detects line
-        SightLeft();
+        HardLeft();
         break;
 
     case 1000:    //  slightly left sensor detects line
@@ -132,9 +163,15 @@ void loop (){
         break;
 
     case 1:
-        SlightRight();
+        HardRight();
         break;
 
+    case 1100:
+        left_acute = 1;
+
+        
+    case 110:
+        right_acute = 1;
     
         
     
